72ff50ae46b6b8cbd26b395b3f95ad828461ad87
YugwonWon <linky1584@gmail.com>
2022-07-17 18:27:09 +0900

    이미지를 처리하도록 하는 box위젯과 그리드 위젯 테스트
    - 전체 메인 레이아웃은 BoxLayout으로 구성
    - 이미지를 처리하는 위젯은 FloatLayout을 불러와서 처리함
    - 검색을 위한 레이아웃은 Gridlayout을 이용하였음

--- main.py
+++ main.py
@@ -3,34 +3,68 @@
 import os
 import kivy
 kivy.require('2.1.0')
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+from kivy.app import App
+from kivy.graphics import Color, Rectangle
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import AsyncImage
+
+
 from kivy.properties import ObjectProperty, StringProperty
 
 fontName = 'NanumGothic.ttf'
 
+class RootWidget(BoxLayout):
+    pass
 
-class Controller(FloatLayout):
-    '''Create a controller that receives a custom widget from the kv lang file.
 
-    Add an action to be called from the kv lang file.
-    '''
-    label_wid = ObjectProperty()
-    info = StringProperty()
-    def bttn01_clicked(self):
-        layout = FloatLayout()
-        button01 = Button(text = 'Button 01', size_hint =(.5, .0))
-        layout.add_widget(button01)
-        self.label_wid.text = 'clicked the button'
-        self.info = 'New info text'
+class MainLayout(FloatLayout):
 
-class ControllerApp(App):
+    def __init__(self, **kwargs):
+        # make sure we aren't overriding any important functionality
+        super(MainLayout, self).__init__(**kwargs)
+
+        with self.canvas.before:
+            Color(0, 0, 0, 1)  # green; colors range from 0-1 instead of 0-255
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+
+        self.bind(size=self._update_rect, pos=self._update_rect)
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+        
+class SearchLayout(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(SearchLayout, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='Search Meme: '))
+        self.meme=TextInput(multiline=False)
+        self.add_widget(self.meme)
+        # TODO: 검색을 입력했을 때 처리하는 콜백함수 
+
+class MainApp(App):
 
     def build(self):
-        return Controller(info = 'hello world')
+        root = RootWidget()
+        root.orientation = 'vertical'
+        m = MainLayout()
+        root.add_widget(m)
+        m.add_widget(
+            AsyncImage(
+                source="images/meme.png",
+                size_hint= (1, .5),
+                pos_hint={'center_x':.5, 'center_y':.5}))
+        s = SearchLayout()
+        root.add_widget(s)
 
+        return root
 
 if __name__ == '__main__':
-    ControllerApp().run()
+    MainApp().run()
